576b08d96a3ad0ad937f19c7746ce5bb57077af1
AshwiN <ashwin.triz@gmail.com>
2016-10-14 20:29:50 +0530

    Changed the method, made it more generic. Cured Y and X equal conditions

--- Code/xyIntercept.c
+++ Code/xyIntercept.c
@@ -4,7 +4,7 @@
 void main(void)
 {
 
-	float x[4],y[4], m1,m2,del, delX, delY, xin,yin;
+	float x[4],y[4],detX,detY,dx1,dy1,dx2,dy2,S1,S2, xin=-1,yin=-1,det;
 
 	int i;
 
@@ -23,20 +23,31 @@ void main(void)
 		scanf("%f",&y[i]);
 	}
 
-	
-	m1=(y[1]-y[0])/(x[1]-x[0]);
-	m2=(y[3]-y[2])/(x[3]-x[2]);
-	
-	del= -m1+m2;
+	dx1=x[1]-x[0];
+	dy1=y[1]-y[0];
 
-	delX=(-m1*x[0])+(m2*x[2])+y[0]-y[2];
-	
-	delY=(-m1*y[2])+(m1*m2*x[2])+(m2*y[0])-(m1*m2*x[0]);
+	dx2=x[3]-x[2];
+	dy2=y[3]-y[2];
 
+	S1=(dx1*y[0])-(dy1*x[0]);
+	S2=(dx2*y[2])-(dy2*x[2]);
 	
+	detX=(S1*dx2)-(S2*dx1);
+	detY=(-S2*dy1)+(S1*dy2);
+	det=(-dy1*dx2)+(dx1*dy2);
 	
-	xin= delX/del;
-	yin=delY/del;
+	if(det==0)
+	{
+		printf("\nThe Lines will never intersect or they are the same");
+	}
+
+	else
+	{
+		xin= detX/det;
+		yin=detY/det;
+	}
+
+	//prints neagtive values if the line won't intersect or there infinity such points
 
 	printf("\n\nXintercept = %f \t Yintercept=%f",xin,yin);	
 
